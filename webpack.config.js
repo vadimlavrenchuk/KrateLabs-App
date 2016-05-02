2e477a6f7ab69051aed95bd200d07089ad32ea61
Denis Carriere <carriere.denis@gmail.com>
2016-05-01 20:47:29 -0400

    Add Distro bundle

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     html: './index.html'
   },
   output: {
-      path: path.join(__dirname, './assets'),
+      path: path.join(__dirname, './dist'),
       filename: 'bundle.js'
   },
   resolve: {
